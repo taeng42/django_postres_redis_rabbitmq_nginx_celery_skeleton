614273e7cf7365e8cbb59656e83f4dc0ffe4d551
taeng <hyundong@42seoul.kr>
2023-07-15 21:20:02 +0900

    Dockerfile: install wait-for-it. docker-compose.yml: use wait-for-it, etc: more test

--- taeng/settings.py
+++ taeng/settings.py
@@ -31,6 +31,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
         "127.0.0.1",
         "localhost",
+        "172.25.147.9",
         ]
 
 
--- test_app/views.py
+++ test_app/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.views.decorators.http import require_http_methods
 
 from .tasks import add, mul
 
 import time
+import json
 
-
+@method_decorator(csrf_exempt, name='dispatch')
 def index(request):
+    '''
     print("index", flush=True)
     result = add.delay(1, 100)
     result2 = mul.delay(2, 12)
@@ -18,3 +23,19 @@ def index(request):
 
     ret = result
     return HttpResponse(f"{ret.id} {ret.ready()} {ret.get()} {hashed}<br>{result2.id} {result2.ready()} {result2.get()}")
+    '''
+    result = add.delay(1, 100)
+    result2 = mul.delay(2, 12)
+    hashed = 0
+    ret = result
+    time.sleep(0.01)
+    print(f"{ret.id} {ret.ready()} {ret.get()} {hashed}<br>{result2.id} {result2.ready()} {result2.get()}", flush=True)
+
+    '''
+    print(request.headers)
+    print(json.loads(request.body), flush=True)
+    resp = HttpResponse("done")
+    return resp 
+    '''
+    #return "Hello"
+    return HttpResponse("done", 200)
